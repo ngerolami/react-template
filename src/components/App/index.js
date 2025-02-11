a80e443439e24be9b30f0dfd67f5f11b76375083
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-11 16:03:46 -0300

    Update src/components/App/index.js for Create a new page with a pre-loader on it

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,29 @@
+```jsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NewPage from '../NewPage';
+import PreLoader from '../PreLoader';
+// Import other components as needed
 
-const App = () => <h1>This is a connected react app</h1>;
+/**
+ * App component
+ * Root component of the React application.
+ */
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        {/* Show the pre-loader while the app is loading */}
+        <PreLoader />
+        <Switch>
+          {/* Define routes for existing pages */}
+          <Route path="/new-page" component={NewPage} />
+          {/* Add routes for other pages */}
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
+```
