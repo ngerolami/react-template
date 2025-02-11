da79f54617c0c4ae5d753b21eb2b12fc5079a7f6
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-11 15:26:55 -0300

    Update src/reducers/index.js for Create a new page with a pre-loader on it

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
+```jsx
 import { combineReducers } from 'redux';
+// Import other reducers
+import preLoaderReducer from './preLoader';
 
-import sample from './sample';
-
-export default combineReducers({
-  sample,
+// Combine all reducers
+const rootReducer = combineReducers({
+  // Add other reducers here
+  preLoader: preLoaderReducer, // Add the preloader reducer
 });
+
+export default rootReducer;
+```
