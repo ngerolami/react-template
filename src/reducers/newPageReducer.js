409c32275e35e4902565917dac2dd47160211d5f
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-11 16:05:52 -0300

    Create src/reducers/newPageReducer.js for Create a new page with a pre-loader on it

---
+++ src/reducers/newPageReducer.js
@@ -0,0 +1,20 @@
+```jsx
+/**
+ * A reducer for managing the state of the NewPage component.
+ * This reducer is not currently used, but it can be added to the root reducer if needed.
+ */
+
+const initialState = {
+  // Add any initial state properties for the NewPage component here
+};
+
+const newPageReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Add cases for handling different actions related to the NewPage component
+    default:
+      return state;
+  }
+};
+
+export default newPageReducer;
+```
