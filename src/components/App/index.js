42f18f46b7f6d46d7ca57455cb548297c60d9b2a
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-11 15:13:27 -0300

    Update src/components/App/index.js for Create a new page with a pre-loader on it

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Preloader from '../Preloader';
 
-const App = () => <h1>This is a connected react app</h1>;
+// Import other components and functionality as needed
 
-export default App;
+/**
+ * Root component of the application
+ * @param {Object} props - Component props
+ * @param {boolean} props.isLoading - Pre-loader loading state
+ */
+const App = ({ isLoading }) => {
+  return (
+    <div>
+      {/* Render pre-loader if loading */}
+      {isLoading && <Preloader />}
+
+      {/* Render other components and functionality */}
+      {/* ... */}
+    </div>
+  );
+};
+
+// Map Redux state to component props
+const mapStateToProps = (state) => ({
+  isLoading: state.preloader.isLoading,
+});
+
+export default connect(mapStateToProps)(App);
