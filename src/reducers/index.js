1b423202d0c6876350d98259c8d4bb1a3e75b9ea
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-11 16:00:00 -0300

    Update src/reducers/index.js for Create a new page with a pre-loader on it

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,16 @@
+```javascript
 import { combineReducers } from 'redux';
+import preLoaderReducer from './preLoader';
+import newPageReducer from './newPage';
 
-import sample from './sample';
+// Remove the sample reducer if it's not used elsewhere
+// import sampleReducer from './sample';
 
-export default combineReducers({
-  sample,
+const rootReducer = combineReducers({
+  preLoader: preLoaderReducer,
+  newPage: newPageReducer,
+  // Add any other reducers you have
 });
+
+export default rootReducer;
+```
