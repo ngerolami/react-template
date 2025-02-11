c944c22b1d3c8c7698674b7a2ecaa86203b4a711
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-11 15:24:58 -0300

    Create src/components/NewPage/index.js for Create a new page with a pre-loader on it

---
+++ src/components/NewPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+/**
+ * NewPage component
+ *
+ * This component represents the new page to be added to the application.
+ * You can add your page content and functionality here.
+ */
+const NewPage = () => {
+  return (
+    <div className="new-page">
+      <h1>New Page</h1>
+      {/* Add your page content here */}
+    </div>
+  );
+};
+
+export default NewPage;
