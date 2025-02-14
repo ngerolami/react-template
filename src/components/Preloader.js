43033a4deeaef7b91ab5bf2581181b6c63b93024
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-14 14:43:00 -0300

    Create src/components/Preloader.js

---
+++ src/components/Preloader.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+export default Preloader;
