bbaf91384ac5a84a4a6bd83be294856399e3bfeb
Nicolás Gerolami <13407943+ngerolami@users.noreply.github.com>
2025-02-17 23:19:24 -0300

    Create src/components/LoadingIndicator.js

---
+++ src/components/LoadingIndicator.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+const LoadingIndicator = () => {
+  return (
+    <div className="loading-indicator">
+      <div className="spinner"></div>
+      <span>Loading...</span>
+    </div>
+  );
+};
+export default LoadingIndicator;
